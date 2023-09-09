09ae07106ab075b5583b8cd9cbbae0bc93aec772
hoakhuu <112261849+hoakhuu@users.noreply.github.com>
2023-09-09 15:42:43 +0700

    HoaKhuu

--- Project_FurnitureShop_PM/Project_FurnitureShop_PM/Controllers/HomeController.cs
+++ Project_FurnitureShop_PM/Project_FurnitureShop_PM/Controllers/HomeController.cs
@@ -6,11 +6,11 @@ namespace Project_FurnitureShop_PM.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+           
         }
 
         public IActionResult Index()
@@ -18,10 +18,7 @@ namespace Project_FurnitureShop_PM.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
